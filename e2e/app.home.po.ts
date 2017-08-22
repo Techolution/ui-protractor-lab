120760c1485e5f58e3becb116df9e814f85e77b0
originalusrnm <jshornbuckle@gmail.com>
2017-08-22 15:17:51 -0400

    update

--- e2e/app.home.po.ts
+++ e2e/app.home.po.ts
@@ -2,27 +2,11 @@ import { browser, by, element } from 'protractor';
 
 export class UiProtractorLabHomePage {
   navigateTo() {
-    return browser.get('/');
+    
   }
 
   getHeader() {
     return element(by.css('h1')).getText();
-  }
-
-  getAddButton() {
-    return element(by.css('.btn'));
-  }
-
-  getAddInput() {
-    return element(by.tagName('input'));
-  }
-
-  getAsynBtn() {
-    return element(by.css('#asyncBtn'));
-  }
-
-  getNamesList() {
-    return element.all(by.tagName('p'));
   }
 
 }
